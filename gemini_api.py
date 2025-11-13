9d3d406c525a9ca52b6f6b1b12242611f735845e
TEGAY VIKTORIYA PAVLOVNA <victoria.t@g.skku.edu>
2025-11-13 06:34:06 +0000

    Add Google GenAI package

---
+++ gemini_api.py
@@ -0,0 +1,14 @@
+from google import genai
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
+
+# The client gets the API key from the environment variable `GEMINI_API_KEY`.
+client = genai.Client()
+
+response = client.models.generate_content(
+    model="gemini-2.5-flash", contents="Explain how AI works in a few words"
+    )
+print(response.text)
